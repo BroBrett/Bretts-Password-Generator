8d378e6552efd8e30daa2276f37a6ee7ab2c6bcd
brett hockridge <bretthockridge98@gmail.com>
2022-01-07 19:35:28 -0800

    added letters, numbers, and symbols

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//create arrays for lowercase, uppercase, numbers, special characters
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"];
+
+// user input for the Password
+
+var inputLength = "";
+var inputLowerCase;
+var inputUpperCase;
+var inputNumbers;
+var inputSpecialChar;
+
+function generatePassword() {
+  
+
+  
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
